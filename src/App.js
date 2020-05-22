3b31390abc35b86908257ff57177c8d3af90e699
Mayar Elabbasy <mayarelabbasy2016@gmail.com>
2020-05-22 02:42:33 +0200

    refactor the code and used props

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Menu from './components/MenuComponent';
+import { DISHES } from './shared/Dishes';
 
-function App() {
-  return (
-    <div>
-      <Menu />
-    </div>
-  );
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+render(){
+    return (
+      <div>
+        <Menu dishes = {this.state.dishes}/>
+      </div>
+    );
+  }
 }
 
 export default App;
