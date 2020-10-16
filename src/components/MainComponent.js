b249f6d9db039f6a66cfb72f84e531b90d30b275
Mayar Elabbasy <mayarelabbasy2016@gmail.com>
2020-10-16 20:23:05 +0200

    fetch handling errors

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -41,7 +41,7 @@ class MainComponent extends Component{
       return(
         <Home dish={this.props.dishes.dishes.filter((dish) => dish.featured)[0]}
         dishesLoading={this.props.dishes.isLoading}
-        dishErrMess={this.props.dishes.errMess}
+        dishesErrMess={this.props.dishes.errMess}
         promotion={this.props.promotions.promotions.filter((promo) => promo.featured)[0]}
         promoLoading={this.props.promotions.isLoading}
         promoErrMess={this.props.promotions.errMess}
